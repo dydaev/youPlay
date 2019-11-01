a8dbe99a5d84996fd12eb3d67986cf895860e7a8
romaChe <dydaev@gmail.com>
2019-11-01 11:00:23 +0200

    fix full screen for ios

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,47 +1,48 @@
 export default {
-  useFullScreenMode: async (isActive: boolean) => {
-    try {
-      if (typeof window.orientation !== 'undefined' && document.fullscreenEnabled) {
-        // addEventListener('click', async () => {
-        // tslint:disable-next-line:one-variable-per-declaration
+	useFullScreenMode: async (isActive: boolean) => {
+		try {
+			if (typeof window.orientation !== "undefined" && document.fullscreenEnabled) {
+				// addEventListener('click', async () => {
+				// tslint:disable-next-line:one-variable-per-declaration
 
-        const elem: any = document.documentElement;
-        //   rfs = isActive
-        //     ? el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen
-        //     : el.exitFullscreen;
-        // const res: Promise<any> = await rfs.call(el);
-        if (isActive && !document.fullscreen) {
-          elem.requestFullscreen().catch((err: any) => {
-            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-          });
-        }
+				const elem: any = document.documentElement;
+				//   rfs = isActive
+				//     ? el.requestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen
+				//     : el.exitFullscreen;
+				// const res: Promise<any> = await rfs.call(el);
+				if (isActive && !document.fullscreen) {
+					elem.requestFullscreen() ||
+						elem.webkitRequestFullScreen() ||
+						elem.mozRequestFullScreen() ||
+						elem.msRequestFullscreen();
+				}
 
-        if (!isActive && document.fullscreen) {
-          document.exitFullscreen();
-        }
-        // console.log(res);
-        // });
-      }
-    } catch (e) {
-      console.log('Can not use full screen mode(.');
-    }
-  },
-  usePlaingInTry: (isSavePlaying: any, setPlaying: any) => {
-    try {
-      window.addEventListener(
-        'visibilitychange',
-        () => {
-          if (isSavePlaying) {
-            console.log('playing after lost the focus');
-            setPlaying(true);
-          } else {
-            setPlaying(false);
-          }
-        },
-        false,
-      );
-    } catch (e) {
-      console.log('Can not playing in tray(.', e);
-    }
-  },
+				if (!isActive && document.fullscreen) {
+					document.exitFullscreen();
+				}
+				// console.log(res);
+				// });
+			}
+		} catch (e) {
+			console.log("Can not use full screen mode(.");
+		}
+	},
+	usePlaingInTry: (isSavePlaying: any, setPlaying: any) => {
+		try {
+			window.addEventListener(
+				"visibilitychange",
+				() => {
+					if (isSavePlaying) {
+						console.log("playing after lost the focus");
+						setPlaying(true);
+					} else {
+						setPlaying(false);
+					}
+				},
+				false,
+			);
+		} catch (e) {
+			console.log("Can not playing in tray(.", e);
+		}
+	},
 };
